2a6e1de3c35aa3b2b432971b46f466d3a3f40d0c
jha jyotsna <70332102+jhajyotsna@users.noreply.github.com>
2023-06-26 13:20:12 +0530

    making changes on contact

--- src/Components/contact/Contact.jsx
+++ src/Components/contact/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
           <article className="contact__option">
             <BsWhatsapp className='contact__option-icon' />
             <h4>WhatsApp</h4>
-            <h5>+91-7758855557</h5>
+            <h5>+91-7758855556</h5>
             <a href="https://api.whatsapp.com/send?phone+917758855556" target="_blank">
               Send a message
             </a>
